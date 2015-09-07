0b4d894683b0fabd2f5867ba1521f441a0bd57f9
Nick Klauer <klauer@gmail.com>
2015-09-07 13:44:21 -0500

    add test for service call http client

--- wls/client_test.go
+++ wls/client_test.go
@@ -1,6 +1,7 @@
 package wls
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httptest"
@@ -50,17 +51,48 @@ func CreateTestServerResourceRouters() *mux.Router {
 	return r
 }
 
-//func TestServerResourceRelatedCalls(t *testing.T) {
-//	ts := httptest.NewServer(CreateTestServerResourceRouters())
-//	defer ts.Close()
-//	service := new(ServerService)
-//
-//	s, err := service.Servers()
-//	if err != nil {
-//		t.Fail()
-//	}
-//	if len(s) != 2 {
-//		t.Fail()
-//	}
-//
-//}
+func TestServerResourceRelatedCalls(t *testing.T) {
+	ts := httptest.NewServer(CreateTestServerResourceRouters())
+	defer ts.Close()
+	service := new(ServerService)
+
+	service.Environment.ServerUrl = ts.URL
+	service.Environment.Username = "user"
+	service.Environment.Password = "pass"
+	s, err := service.Servers()
+	if err != nil {
+		t.Fail()
+	}
+	if len(s) != 2 {
+		t.Fail()
+	}
+
+	server, err := service.Server("adminserver")
+	if err != nil {
+		t.Fail()
+	}
+	var server_json_tests = []struct {
+		in  string
+		out string
+	}{
+		{server.Name, "adminserver"},
+		{server.ClusterName, ""},
+		{server.State, "RUNNING"},
+		{server.CurrentMachine, "machine-0"},
+		{server.WeblogicVersion, "WebLogic Server 12.1.1.0.0 Thu May 5 01:17:16 2011 PDT"},
+		{fmt.Sprint(server.OpenSocketsCurrentCount), "2"},
+		{server.Health, "HEALTH_OK"},
+		{fmt.Sprint(server.HeapSizeCurrent), "536870912"},
+		{fmt.Sprint(server.HeapFreeCurrent), "39651944"},
+		{server.JavaVersion, "1.6.0_20"},
+		{server.OsName, "Linux"},
+		{server.OsVersion, "2.6.18-238.0.0.0.1.el5xen"},
+		{fmt.Sprint(server.JvmProcessorLoad), "0.25"},
+	}
+
+	for _, tt := range server_json_tests {
+		if tt.in != tt.out {
+			t.Errorf("want %q, got %q", tt.out, tt.in)
+		}
+	}
+}
